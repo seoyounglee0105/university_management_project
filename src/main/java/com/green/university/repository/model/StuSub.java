d0b56616545cf3c1be62c9032ba6d87681262ac1
DevDogFe <jhkim900726@gmail.com>
2023-04-21 15:42:27 +0900

    엔티티모델 생성

---
+++ src/main/java/com/green/university/repository/model/StuSub.java
@@ -0,0 +1,11 @@
+package com.green.university.repository.model;
+
+import lombok.Data;
+
+@Data
+public class StuSub {
+
+	private Integer studentId;
+	private Integer subjectId;
+	private String grade;
+}
---
+++ src/main/java/com/green/university/repository/model/Tuition.java
@@ -0,0 +1,12 @@
+package com.green.university.repository.model;
+
+import lombok.Data;
+
+@Data
+public class Tuition {
+
+	private Integer studentId;
+	private String semester;
+	private String status;
+	
+}
